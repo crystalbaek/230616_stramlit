7140d8021d330cbbad980a60165b9b6d91bcdb7c
crystalbaek <bfilkl730@gmail.com>
2023-06-16 12:33:39 +0900

    0616

--- app.py
+++ app.py
@@ -49,30 +49,3 @@ text = """
 
 """
 
-st.write("컴포넌트")
-# 위-아래 한 줄로만...
-
-st.write("🤣")
-cols = st.columns(2)  # 컬럼 리스트
-cols[0].write("🤣")
-cols[1].write("🤣")
-cols = st.columns(3)
-
-# 🍉 -> n 등분 -> 3등분
-cols[0].write("🍉")
-cols[1].write("🍉")
-cols[-1].write("🍉")
-cols = cols[0].columns(3) # 열의 열인 거임
-cols[0].write("🍉")
-cols[1].write("🍉")
-cols[-1].write("🍉")
-col1, col2 = st.columns(2)  # 리스트 언패킹
-col1.write("왼쪽 열")
-col2.write("오른쪽 열")
-
-with col1: # col1을 기준으로 streamli을 써주겠다
-    # 블록 (:) 을 열면 -> 이 안에서는 streamlit 기능 실행시 col1에 종속
-    st.write("왼쪽")
-with col2: # col1을 기준으로 streamli을 써주겠다
-    # 블록 (:) 을 열면 -> 이 안에서는 streamlit 기능 실행시 col2에 종속
-    st.write("왼쪽")
