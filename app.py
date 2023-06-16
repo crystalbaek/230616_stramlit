f98465778c469cafba1387bb804c65c1b4b9d006
crystalbaek <bfilkl730@gmail.com>
2023-06-16 10:50:25 +0900

    0616

--- app.py
+++ app.py
@@ -14,3 +14,39 @@ st.image("img/img.jpeg")  # 파일 경로 (app.py)
 st.image(image="img/img.jpeg")  # 키워드를 사용해서...
 st.image("img/img.jpeg", use_column_width=True)  # 파일 경로 (app.py)
 st.image("img/img.jpeg", width=100)  # 파일 경로 (app.py)
+
+# 마크다운
+# https://heropy.blog/2017/09/30/markdown/
+
+# st.write / st.markdown
+# st.write -> 입력하는 것에 맞춰서 알아서 결정 -> 마크다운
+# st.markdown -> 명백하게 마크다운을 실행하겠다
+
+# 제목 마크다운
+st.write("""
+# 가장 큰 제목  (h1 - headline1 - st.title)
+## 그 다음 큰 제목 (h2 - headline2 - st.header)
+### 그것보단 작은 제목 <- 대부분 여기까지만 씀 (h3 - headline3 - st.subheader)
+#### 좀 더 작은 제목 (h4)
+##### 이건 없겠지? (h5)
+###### 이것도 있나? (h6)
+####### 이건 없어.
+""")  # 문자열로 넣으면 마크다운
+
+# 서식
+text = """
+기울임 : *별표* 또는 _언더바_를 하나씩 써주면
+ 
+진하게(bold) : **별표** 또는 __언더바__를 두개씩 써주면
+ 
+기울임 + 진하게(bold) : ***별표*** 또는 ___언더바___를 세개씩 써주면 
+
+취소선 : ~물결표~
+
+밑줄 : <U>밑줄</u>
+
+형광펜 : <mark>형광펜</mark>
+"""
+# st.write(text)
+# 태그를 허용하는 옵션
+st.markdown(text, unsafe_allow_html=True)
