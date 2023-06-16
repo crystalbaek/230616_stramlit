c99ad81abedad10bc6bd722cafcae13a5bc16978
crystalbaek <bfilkl730@gmail.com>
2023-06-16 10:29:41 +0900

    0616

--- app.py
+++ app.py
@@ -10,4 +10,7 @@ st.write("내가 만든 페이지, 너를 위해 구웠지")
 st.video("https://www.youtube.com/watch?v=SaCheA6Njc4")
 st.image("https://cdn.pixabay.com/photo/2023/06/02/14/12/woman-8035772_1280.jpg")  # 인터넷 링크
 st.image("https://i.imgur.com/FD6kOUY.jpeg")
-st.image("img/img.jpeg")
+st.image("img/img.jpeg")  # 파일 경로 (app.py)
+st.image(image="img/img.jpeg")  # 키워드를 사용해서...
+st.image("img/img.jpeg", use_column_width=True)  # 파일 경로 (app.py)
+st.image("img/img.jpeg", width=100)  # 파일 경로 (app.py)
