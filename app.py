15c4ea19ae9bbedc790b88f58c6b7c39cfcc7af8
crystalbaek <bfilkl730@gmail.com>
2023-06-16 10:21:12 +0900

    0616

--- app.py
+++ app.py
@@ -5,5 +5,10 @@ import streamlit as st
 st.title("나의 파이썬 웹 페이지")
 st.header("수업 8일차에 만들었어요")
 st.subheader("그래도 잘 만들었죠?")
-st.write("내가 만든 페이지, 너를 위해 구웠지")
 
+
+st.video("https://www.youtube.com/watch?v=SaCheA6Njc4")
+st.image("https://cdn.pixabay.com/photo/2023/06/02/14/12/woman-8035772_1280.jpg")  # 인터넷 링크
+st.write("내가 만든 페이지, 너를 위해 구웠지")
+st.image("https://i.imgur.com/FD6kOUY.jpeg")
+# st.image("img")
