22b5fd913df06dfe4a72611a6c53a6273e7df142
crystalbaek <bfilkl730@gmail.com>
2023-06-16 09:37:16 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+# st. -> ctrl + space -> 다양한 기능(함수, 메소드)을 가지고 있다
+
+st.title("나의 파이썬 웹 페이지")
+st.header("수업 8일차에 만들었어요")
+st.subheader("그래도 잘 만들었죠?")
+st.write("내가 만든 페이지, 너를 위해 구웠지")
+
